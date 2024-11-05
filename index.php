f7ee7a8176d51b02cac42e8c0162deae168d24a9
Enrico Webers <enricowebers@gmail.com>
2024-11-05 14:48:00 +0000

    Output Tests

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 echo '
-<form action="excelOutput1.php" method="post">
+<form action="excelOutput.php" method="post">
   <input type="hidden" name="excelOutput" value="Yes">
   <button type="submit" class="pageheader-blue mt-5 px-4 py-2" name="export_excel">
     FX Rates to EUR
